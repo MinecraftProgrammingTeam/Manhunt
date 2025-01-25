be54a655a54d8785857846079cafae61029e6778
X_huihui <2519350300@qq.com>
2025-01-25 22:37:11 +0800

    Init
    Init

---
+++ src/main/java/top/mpt/huihui/manhunt/Manhunt.java
@@ -0,0 +1,17 @@
+package top.mpt.huihui.manhunt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Manhunt extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
